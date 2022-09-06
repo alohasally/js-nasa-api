a68fee41d15507b4f6801ff027451cbca1c66895
alohasally <peacheeks@gmail.com>
2022-09-06 12:10:11 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+
+// NASA API
+
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+function updateDom(){
+    resultsArray.forEach((result) => {
+        // Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        // Link
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View Full Image';
+        link.target = '_blank'
+        // Image 
+        const image = document.createElement('img');
+        image.src = result.url;
+        image.alt = 'NASA Picture of the Day';
+        image.loading = 'lazy';
+        image.classList.add('card-img-top');
+    });
+}
+
+
+// Get 10 Images from NASA API
+async function getNasaPictures(){
+    try{
+        const response = await fetch(apiUrl);
+        resultsArray = await response.json();
+        updateDom();
+    } catch (error){
+        // Catch Error Here
+    }
+}
+
+// On Load
+getNasaPictures();
